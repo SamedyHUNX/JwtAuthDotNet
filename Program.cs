c993d06e33cccd84ef534ce69a1275544ba959bd
samedyhunx <samedy.hunx@gmail.com>
2026-01-09 16:39:18 +0700

    database connection using postsql

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using JwtAuthDotNet.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<UserDbContext>(options => options.UseNpgsql(connString));
+
+// Build the app
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
