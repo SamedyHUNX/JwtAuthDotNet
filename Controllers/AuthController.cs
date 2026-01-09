ffed5f5539b20a9f0d3163cf16404ecb3d9949c0
samedyhunx <samedy.hunx@gmail.com>
2026-01-09 15:47:08 +0700

    createtoken using jwt

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,14 +1,18 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using JwtAuthDotNet.Dtos;
 using JwtAuthDotNet.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JwtAuthDotNet.Controllers
 {
     [Route("api/auth")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IConfiguration configuration) : ControllerBase
     {
         public static User user = new();
 
@@ -20,8 +24,57 @@ namespace JwtAuthDotNet.Controllers
             
             // Store the hashed password
             user.PasswordHash = hashedPassword;
+            user.Username = request.Username;
             
             return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Invalid credential");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            // Claims: what you want to include in the token payload
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            // Get secret key from configuration (configuration injection)
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value!)
+            );
+
+            // Signing credentials
+            // Length 64 bytes (characters): 512 bits
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            // Token descriptor
+            var tokenDescriptor = new JwtSecurityToken
+            (
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
         }
     }
 }
